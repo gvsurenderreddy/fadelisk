5e422d3f2408ccdecd9dbd40de4de185d6331023
Patrick M. Jordan <patrick@appliedneural.net>
2011-02-20 02:50:36 -0500

    Configuration: Provide more dictionary functionality.

--- lib/python/fadelisk/conf.py
+++ lib/python/fadelisk/conf.py
@@ -19,11 +19,26 @@ class ConfDict(object):
     def __getitem__(self, key):
         return self.data[key]
 
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __contains__(self, key):
+        return key in self.data
+
     def __repr__(self):
         return repr(self.data)
 
     def __str__(self):
         return str(self.data)
+
+    def iteritems(self):
+        return self.data.iteritems()
+
+    def items(self):
+        return self.data.items()
 
     def get(self, key, default=None):
         return self.data.get(key, default)
