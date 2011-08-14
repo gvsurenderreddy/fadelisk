3dd18f2107a633073b346068fd93ccfece5f8512
Patrick M. Jordan <patrick@appliedneural.net>
2011-08-14 19:10:41 -0400

    conf.py: Derive dynamic confs from dict. Guard some update funtions from updating a conf that may be volatile. Assure thread-safety.

--- lib/python/fadelisk/conf.py
+++ lib/python/fadelisk/conf.py
@@ -1,105 +1,107 @@
 
 from __future__ import with_statement
+from __future__ import print_function
 
 import os
 import sys
 import yaml
 import json
+import threading
 
 class ConfNotFoundError(Exception):
-     def __init__(self, *args):
-         Exception.__init__(self, *args)
+    def __init__(self, *args):
+        Exception.__init__(self, *args)
+
 
 class ConfFormatError(Exception):
-     def __init__(self, *args):
-         Exception.__init__(self, *args)
+    def __init__(self, *args):
+        Exception.__init__(self, *args)
 
-class ConfDict(object):
-    def __init__(self):
-        self.data = {}
 
-    def data(self):
-        return self.data
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    def __iter__(self):
-        return iter(self.data)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __contains__(self, key):
-        return key in self.data
-
-    def __repr__(self):
-        return repr(self.data)
-
-    def __str__(self):
-        return str(self.data)
-
-    def iteritems(self):
-        return self.data.iteritems()
-
-    def items(self):
-        return self.data.items()
-
-    def get(self, key, default=None):
-        return self.data.get(key, default)
-
-    def setdefault(self, key, value):
-        self.data.setdefault(key, value)
-
-    def soft_set(self, key, value):
-        self.data.setdefault(key, value)
-
+class ConfDict(dict):
     def soft_update(self, data):
         for key, value in data.items():
-            self.soft_set(key, value)
+            self.setdefault(key, value)
 
-class ConfYAML(ConfDict):
-    def __init__(self, filename, ignore_changes=False):
+    def replace(self, data):
+        lock = threading.Lock()
+        with lock:
+            self.clear()
+            self.update(data)
+
+
+class ConfDynamicDict(ConfDict):
+    def __init__(self, ignore_changes=False):
         ConfDict.__init__(self)
-        self.filename = filename
         self.ignore_changes = ignore_changes
         self.timestamp = None
+
+    def abort_if_dynamic(self):
+        if self.ignore_changes:
+            return
+        raise RuntimeError, 'Attempted to alter a dynamic configuration'
+
+    def __setitem__(self, key, value):
+        self.abort_if_dynamic()
+        ConfDict.__setitem__(self, key, value)
+
+    def setdefault(self, key, value):
+        self.abort_if_dynamic()
+        ConfDict.setdefault(self, key, value)
+
+    def soft_update(self, data):
+        self.abort_if_dynamic()
+        ConfDict.soft_update(self, data)
+
+    def _replace(self, data):
+        ConfDict.replace(self, data)
+
+    def replace(self, data):
+        self.abort_if_dynamic()
+        self._replace(data)
+
+    # TODO: Also protect:
+    # pop
+    # popitem
+    # __delitem__
+
+class ConfYAML(ConfDynamicDict):
+    def __init__(self, filename, ignore_changes=False):
+        ConfDynamicDict.__init__(self, ignore_changes)
+        self.filename = filename
         self.refresh()
 
     def __getitem__(self, key):
         if not self.ignore_changes:
             self.refresh()
-        return ConfDict.__getitem__(self, key)
+        return ConfDynamicDict.__getitem__(self, key)
 
     def get(self, key, default=None):
         if not self.ignore_changes:
             self.refresh()
-        return ConfDict.get(self, key, default)
+        return ConfDynamicDict.get(self, key, default)
 
     def refresh(self):
         mtime = os.stat(self.filename).st_mtime
         if mtime != self.timestamp:
             with open(self.filename) as yaml_file:
-                self.data = yaml.load(yaml_file)
-            if self.data == None:
-                self.data = {}
-            if not isinstance(self.data, dict):
+                data = yaml.load(yaml_file) or {}
+            if not isinstance(data, dict):
                 raise ConfFormatError('ConfYAML target must be a dictionary')
+            self._replace(data)
             self.timestamp = mtime
 
 
-class ConfJSON(ConfDict):
+class ConfJSON(ConfDynamicDict):
     def __init__(self, filename, ignore_changes=False):
-        ConfDict.__init__(self)
+        ConfDynamicDict.__init__(self, ignore_changes)
         self.filename = filename
-        self.ignore_changes = ignore_changes
-        self.timestamp = None
         self.refresh()
 
     def __getitem__(self, key):
         if not self.ignore_changes:
             self.refresh()
-        return ConfDict.__getitem__(self, key)
+        return ConfDynamicDict.__getitem__(self, key)
 
     def refresh(self):
         mtime = os.stat(self.filename).st_mtime
