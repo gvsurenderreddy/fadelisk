60bfd42b10118f6e04b14b0d1715772d1d602818
Patrick M. Jordan <patrick@appliedneural.net>
2015-09-29 16:02:16 -0400

    In very temporary wrapper script, try to find non-absolute script filename in $PATH.

--- bin/fadelisk-alt-python-version.sh
+++ bin/fadelisk-alt-python-version.sh
@@ -3,5 +3,24 @@
 INTERPRETER="python2.6"
 SCRIPT="fadelisk.real"
 
+if ! echo $SCRIPT | egrep -q '^/'
+then
+    # Script does not have an absolute path. Try to find it in $PATH.
+    if which $SCRIPT >/dev/null
+    then
+	SCRIPT=$(which $SCRIPT)
+    else
+	echo "Script $SCRIPT could not be found in path"
+	exit 1
+    fi
+else
+    # Otherwise, make sure it exists.
+    if ! [ -e $SCRIPT ]
+    then
+	echo "Script $SCRIPT does not exist"
+	exit 1
+    fi
+fi
+
 exec $INTERPRETER $SCRIPT $@
 
